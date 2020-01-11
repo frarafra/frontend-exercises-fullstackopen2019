cc769c52bb40f4d1293b609a9603872c4bf8f205
Francesc Ara <cesc78@gmail.com>
2020-01-11 19:55:19 +0100

    Update connection to backend for production stage

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,21 +1,21 @@
 import axios from 'axios'
 
-const API_URL = 'http://localhost:3001/persons'
+const baseUrl = '/api/persons'
 
 const getAll = () => {
-  return axios.get(API_URL).then(response => response.data)
+  return axios.get(baseUrl).then(response => response.data)
 }
 
 const create = newObject => {
-  return axios.post(API_URL, newObject).then(response => response.data)
+  return axios.post(baseUrl, newObject).then(response => response.data)
 }
 
 const update = (id, newObject) => {
-  return axios.put(`${API_URL}/${id}`, newObject).then(response => response.data)
+  return axios.put(`${baseUrl}/${id}`, newObject).then(response => response.data)
 }
 
 const remove = (id) => {
-  return axios.delete(`${API_URL}/${id}`)
+  return axios.delete(`${baseUrl}/${id}`)
 }
 
 export default { 

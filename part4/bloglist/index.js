ab8ececd79187d7e7172b7b8ce8838f78307c359
Francesc Ara <cesc78@gmail.com>
2020-02-24 13:08:08 +0100

    First commit

---
+++ part4/bloglist/index.js
@@ -0,0 +1,49 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const morgan = require('morgan')
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl = process.env.MONGODB_URI
+mongoose.connect(mongoUrl, { useNewUrlParser: true })
+
+morgan.token('req-body', req => {
+  return JSON.stringify(req.body)
+})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-body'))
+app.use(cors())
+app.use(bodyParser.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
